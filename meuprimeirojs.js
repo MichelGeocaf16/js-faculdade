85ce01a8a0b8c4d51ec09088bc86a1ff9ecc8f3a
M Geocaf <mgeocaf.developer@gmail.com>
2022-08-28 23:40:05 -0300

    first commit

---
+++ meuprimeirojs.js
@@ -0,0 +1,39 @@
+/*n1 =2;
+n2 = 3;
+
+console.log(n1 - n2) */
+
+/*x = 1;
+//expressões
+expressao_1 = (x%2 ==0)// é par?
+expressao_2 = (x%2 ==1)// é impar?
+console.log(expressao_1);
+console.log(expressao_2);
+
+x = 1;
+expressao_1 = (x % 2 == 0)
+expressao_2 = (x % 2 == 1)
+console.log(expressao_1 && expressao_2)*/
+
+/*nota = 10;
+
+if(nota >= 7){
+
+    console.log("Aprovado");
+}else{
+    console.log("Reprovado");
+}*/
+
+
+sal = 1200;
+
+horastrabalho= 220;
+h_extra = 12;
+
+extras_recebidas = (sal / horastrabalho)* h_extra;
+
+console.log(" horas extras: " +extras_recebidas);
+
+vencimentos = extras_recebidas + sal;
+inss = 0.8 * 10/100;
+
